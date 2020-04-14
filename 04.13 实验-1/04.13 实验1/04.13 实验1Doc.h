d4375e1a84ac589ffe31ac223dcc55719aa0ef8a
zhanghuanjian <2027663096@qq.com>
2020-04-14 13:40:13 +0800

    2020.04.13实验-1

---
+++ 04.13 实验-1/04.13 实验1/04.13 实验1Doc.h
@@ -0,0 +1,49 @@
+
+// 04.13 实验1Doc.h : CMy0413实验1Doc 类的接口
+//
+
+
+#pragma once
+
+
+class CMy0413实验1Doc : public CDocument
+{
+protected: // 仅从序列化创建
+	CMy0413实验1Doc();
+	DECLARE_DYNCREATE(CMy0413实验1Doc)
+
+// 特性
+public:
+
+// 操作
+public:
+	CRect m_rect;
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CMy0413实验1Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
